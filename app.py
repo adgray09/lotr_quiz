2fab52cd968e204ae1b842c17fe449773fe41302
adgray09 <alexandergray37@yahoo.com>
2019-11-18 22:11:16 -0800

    more questions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 correct_answers = []
 incorrect_answers = []
 
-
+#QUESTIONS
 def question1():
     user_input = input(''' 1. Where is Legolas from?
                     a. Mirkwood
@@ -26,7 +26,7 @@ def question2():
         incorrect_answers.append("number 2")
     
 def question3():
-    user_input = input('''What is Gollums original name?
+    user_input = input('''3. What is Gollums original name?
                     a. Marcho
                     b. Deagol
                     c. Smeagol
@@ -38,12 +38,44 @@ def question3():
         correct_answers.append("number 3")
     else:
         incorrect_answers.append("number 3")
+        
+def question4():
+    user_input = input('''4. How many wizards are there in middle earth?
+                    a. 2
+                    b. 4
+                    c. 5
+                    d. 6
+                    Answer:''')
+    correct_answer = "c"
     
+    if user_input == correct_answer:
+        correct_answers.append("number 4")
+    else:
+        incorrect_answers.append("number 4")
+        
+    
+def question5():
+    user_input = input('''5. Is this a quote from Boromir? 
+                    1 for true 0 for false.
                     
-    
-    #calling questions
+                    "It is a strange fate 
+                    that we should suffer so much
+                    fear and doubt over so small a thing ... 
+                    such a little thing"
+                    
+                    Answer'''
+                    )
+    correct_answer = 1
+    if user_input == correct_answer:
+        correct_answers.append("number 5")
+    else:
+        incorrect_answers.append("number 5")
+        
+#CALLING QUESTION FUNCTIONS
 question1()
-question2()
-question3()
-print(correct_answers)
-print(incorrect_answers)
+#question2()
+#question3()
+#question4()
+#question5()
+print("You got these questions correct: " + str(correct_answers))
+print("You got these questions incorrect: " + str(incorrect_answers))
